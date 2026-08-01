0f1a802395f833512e882d92067ddff67c8d982b
ilyassbouharaoui-eng <ilyassbouharaoui@gmail.com>
2026-08-01 14:58:18 +0200

    Step 5: should_stop

--- model.py
+++ model.py
@@ -57,8 +57,13 @@ def best_split(features, labels, feature_indices):
                 dic['score'] = score
     return dic
 
-# Step 5 - should_stop (not yet solved)
-# TODO: implement
+# Step 5 - should_stop
+def should_stop(labels, depth, max_depth, min_samples_split):
+    """Return True if this node should become a leaf instead of splitting further."""
+    if (len(np.unique(labels)) ==1) or ( depth >= max_depth) or (len(labels) < min_samples_split) :
+        return True
+    else:
+        return False
 
 # Step 6 - leaf_prediction (not yet solved)
 # TODO: implement
